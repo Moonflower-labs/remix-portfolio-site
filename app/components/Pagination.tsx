9ee9bfc3a4618dd974cc334eb62037827a67616b
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2025-04-02 17:02:19 +0100

    added view transition

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router";
 import { useCallback } from "react";
 
-const Pagination = ({ currentPage, totalPages ,setCurrentPage }:PaginationProps) => {
+const Pagination = ({ currentPage, totalPages, setCurrentPage }: PaginationProps) => {
   const navigate = useNavigate();
 
   const pagesArray = Array.from({ length: totalPages }, (_, index) => index + 1);
@@ -14,46 +14,46 @@ const Pagination = ({ currentPage, totalPages ,setCurrentPage }:PaginationProps)
   const handleNextPage = useCallback(() => {
     setCurrentPage((prevPage) => prevPage + 1);
   }, [setCurrentPage]);
- const handleClickPage = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
-  setCurrentPage(parseInt(e.currentTarget.value));
-  navigate(`?page=${parseInt(e.currentTarget.value)}`);
- },[navigate, setCurrentPage]);
+  const handleClickPage = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    setCurrentPage(parseInt(e.currentTarget.value));
+    navigate(`?page=${parseInt(e.currentTarget.value)}`, { viewTransition: true, preventScrollReset: true });
+  }, [navigate, setCurrentPage]);
 
   return (
     <div className="flex justify-center gap-5 mt-8 pb-5">
       <button
-        className={`btn btn-primary ${currentPage === 1 ? 'disabled': ''}`}
+        className={`btn btn-primary ${currentPage === 1 ? 'disabled' : ''}`}
         onClick={handlePreviousPage}
         disabled={currentPage === 1}
       >
         &lt;
       </button>
       {pagesArray.map(page =>
-          <button 
-            className={`btn ${currentPage === page?' disabled':' btn-primary'}`}
-            key={page}
-            onClick={handleClickPage}
-            value={page}
-            disabled={currentPage === page}>
-            {page}
-          </button>
-        )}
+        <button
+          className={`btn ${currentPage === page ? ' disabled' : ' btn-primary'}`}
+          key={page}
+          onClick={handleClickPage}
+          value={page}
+          disabled={currentPage === page}>
+          {page}
+        </button>
+      )}
       <button
-        className={`btn btn-primary ${currentPage === totalPages ? 'hidden': 'block'}`}
+        className={`btn btn-primary ${currentPage === totalPages ? 'hidden' : 'block'}`}
         onClick={handleNextPage}
         disabled={currentPage === totalPages}
       >
         &gt;
       </button>
     </div>
-    
+
   );
 };
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
- 
+
 }
 
 export default Pagination;
